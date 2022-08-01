ac0c90943a64ca1ba032d7cf3a83596964aaf604
sanjeevprabhakar <sanjeevprabhakar2021@gmail.com>
2022-07-31 21:57:20 -0600

    string condition added

---
+++ returnminusoneonstring.js
@@ -0,0 +1,15 @@
+function factFunction(a) {
+    if(typeof a == "string"){
+        return -1
+    }
+    else {
+    var z = 1;
+    for (var i = 1; i <= a; i++) {
+        z = z * i;
+    }
+    return z;
+}
+}
+var result1 = factFunction(6);
+var result2 = factFunction(20);
+console.log(result2);
---
+++ whileloopfactorial.js
@@ -0,0 +1,12 @@
+function factfunction(a){
+    while( x=1<=a){
+fact =1;
+fact = fact * x;
+x++;
+    }
+return fact;
+    } 
+
+    var answerone = factFunction(2);
+var answer2 = factFunction(3);
+console.log(answer2);
