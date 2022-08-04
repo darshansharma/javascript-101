fce0dbd903174c4067a57c1460a076aa5fd6867e
sanjeevprabhakar <sanjeevprabhakar2021@gmail.com>
2022-08-03 22:46:14 -0600

    js project 1.0 continues

--- calculator.js
+++ calculator.js
@@ -1,5 +1,73 @@
-var b = 7;
+var b = '7';
 function sevenfun(){
 
-    document.getElementById("tagone").innerHTML = b;
+    document.getElementById('tagone').value += b;
 }
+
+var a = '6';
+function sixfun(){
+
+    document.getElementById('tagone').value += a;
+}
+
+var c ='5';
+function fivefun(){
+
+    document.getElementById('tagone').value += c;
+}
+
+var d = '4';
+function fourfun(){
+
+    document.getElementById('tagone').value += d;
+}
+
+var e ='3';
+function threefun(){
+
+    document.getElementById('tagone').value += e;
+}
+
+var f = '2';
+function twofun(){
+
+    document.getElementById('tagone').value += f;
+}
+
+var g = '1';
+function onefun(){
+
+    document.getElementById('tagone').value += g;
+}
+
+var h = '+';
+function plusfun(){
+
+    document.getElementById('tagone').value += h;
+}
+
+var i = '-';
+function minusfun(){
+
+    document.getElementById('tagone').value += i;
+}
+
+
+var j = '/';
+function dividefun(){
+
+    document.getElementById('tagone').value += j;
+}
+
+var k = '0';
+function zerofun(){
+
+    document.getElementById('tagone').value += k;
+}
+
+var l = '*';
+function dividefun(){
+
+    document.getElementById('tagone').value += l;
+}
+
