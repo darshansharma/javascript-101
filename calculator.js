01d826e010f6cf301cd020ce6650985c581e0830
Darshan Sharma <darshan.sharma@winabl.com>
2022-08-05 10:48:22 +0530

    indetation fixed and working now

--- calculator.js
+++ calculator.js
@@ -1,106 +1,77 @@
-
-var one = parseInt(document.getElementById("tagone").value);
- 
-
+var one;
 
 function sevenfun(val) {
-    
     document.getElementById("tagone").value += val;
 }
-
 
 function sixfun(val) {
-    
     document.getElementById("tagone").value += val;
 }
-
 
 function fivefun(val) {
-    
     document.getElementById("tagone").value += val;
 }
-
 
 function fourfun(val) {
-    
     document.getElementById("tagone").value += val;
 }
-
 
 function threefun(val) {
-    
     document.getElementById("tagone").value += val;
 }
-
 
 function twofun(val) {
-    
     document.getElementById("tagone").value += val;
 }
-
 
 function onefun(val) {
-    
     document.getElementById("tagone").value += val;
 }
 
-
-
 function plusfun() {
-    var h = '+';
-   document.getElementById("tagone").value = " ";
-document.getElementById("tagone").value += h;
+    one = parseInt(document.getElementById("tagone").value);
+    var h = "+";
+    document.getElementById("tagone").value = " ";
+    document.getElementById("tagone").value += h;
 }
 
-
 function minusfun() {
-    var i = '-';
+    var i = "-";
     document.getElementById("tagone").value += i;
 }
 
-
-
 function dividefun() {
-    var j = '/';
+    var j = "/";
     document.getElementById("tagone").value += j;
 }
 
-
 function zerofun(val) {
-    
     document.getElementById("tagone").value += val;
 }
 
-
 function multiplyfun() {
-    var l = '*';
+    var l = "*";
     document.getElementById("tagone").value += l;
 }
 
-
 function eightfun(val) {
-    
     document.getElementById("tagone").value += val;
 }
-
 
 function ninefun(val) {
-   
     document.getElementById("tagone").value += val;
 }
 
-
 function percentfun() {
-    var o = '%';
+    var o = "%";
     document.getElementById("tagone").value += o;
 }
 
-
 function equalfun() {
-var two = parseInt(document.getElementById("tagone").value);
-    document.getElementById("tagone").value =one + two;
+    var two = parseInt(document.getElementById("tagone").value);
+    document.getElementById("tagone").value = one + two;
 }
 
-function clearfun(){
+function clearfun() {
     document.getElementById("tagone").value = " ";
 }
