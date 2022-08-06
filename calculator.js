788a07d90759fdeceb21d848b7196e5a3a6254cc
sanjeevprabhakar <sanjeevprabhakar2021@gmail.com>
2022-08-05 22:01:14 -0600

    dry principle done and tried little bit css

--- calculator.js
+++ calculator.js
@@ -1,32 +1,10 @@
 var one;
 
-function sevenfun(val) {
+
+function setvalue(val) {
     document.getElementById("tagone").value += val;
 }
 
-function sixfun(val) {
-    document.getElementById("tagone").value += val;
-}
-
-function fivefun(val) {
-    document.getElementById("tagone").value += val;
-}
-
-function fourfun(val) {
-    document.getElementById("tagone").value += val;
-}
-
-function threefun(val) {
-    document.getElementById("tagone").value += val;
-}
-
-function twofun(val) {
-    document.getElementById("tagone").value += val;
-}
-
-function onefun(val) {
-    document.getElementById("tagone").value += val;
-}
 
 function plusfun() {
     one = parseInt(document.getElementById("tagone").value);
@@ -36,40 +14,42 @@ function plusfun() {
 }
 
 function minusfun() {
+    one = parseInt(document.getElementById("tagone").value);
     var i = "-";
+    document.getElementById("tagone").value = " ";
     document.getElementById("tagone").value += i;
 }
 
 function dividefun() {
-    var j = "/";
+    one = parseInt(document.getElementById("tagone").value);
+    var j = '/';
+    document.getElementById("tagone").value = " ";
     document.getElementById("tagone").value += j;
 }
 
-function zerofun(val) {
-    document.getElementById("tagone").value += val;
-}
+
 
 function multiplyfun() {
-    var l = "*";
-    document.getElementById("tagone").value += l;
+    one = parseInt(document.getElementById("tagone").value);
+    document.getElementById("tagone").value = " ";
+   
 }
 
-function eightfun(val) {
-    document.getElementById("tagone").value += val;
-}
-
-function ninefun(val) {
-    document.getElementById("tagone").value += val;
-}
 
 function percentfun() {
-    var o = "%";
-    document.getElementById("tagone").value += o;
+
+    one = parseInt(document.getElementById("tagone").value);
+    
+    document.getElementById("tagone").value = " ";
+    
+    
 }
 
 function equalfun() {
     var two = parseInt(document.getElementById("tagone").value);
     document.getElementById("tagone").value = one + two;
+    
+
 }
 
 function clearfun() {
