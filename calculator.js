d9526ad3dcae97ad32685772859a3e28fadeec82
Darshan Sharma <darshan.sharma@winabl.com>
2022-08-06 11:01:36 +0530

    empty quotes fixed

--- calculator.js
+++ calculator.js
@@ -1,55 +1,44 @@
 var one;
 
-
 function setvalue(val) {
     document.getElementById("tagone").value += val;
 }
 
-
 function plusfun() {
     one = parseInt(document.getElementById("tagone").value);
     var h = "+";
-    document.getElementById("tagone").value = " ";
+    document.getElementById("tagone").value = "";
     document.getElementById("tagone").value += h;
 }
 
 function minusfun() {
     one = parseInt(document.getElementById("tagone").value);
     var i = "-";
-    document.getElementById("tagone").value = " ";
+    document.getElementById("tagone").value = "";
     document.getElementById("tagone").value += i;
 }
 
 function dividefun() {
     one = parseInt(document.getElementById("tagone").value);
-    var j = '/';
-    document.getElementById("tagone").value = " ";
+    var j = "/";
+    document.getElementById("tagone").value = "";
     document.getElementById("tagone").value += j;
 }
 
-
-
 function multiplyfun() {
     one = parseInt(document.getElementById("tagone").value);
-    document.getElementById("tagone").value = " ";
-   
+    document.getElementById("tagone").value = "";
 }
 
-
 function percentfun() {
-
     one = parseInt(document.getElementById("tagone").value);
-    
-    document.getElementById("tagone").value = " ";
-    
-    
+
+    document.getElementById("tagone").value = "";
 }
 
 function equalfun() {
     var two = parseInt(document.getElementById("tagone").value);
     document.getElementById("tagone").value = one + two;
-    
-
 }
 
 function clearfun() {
