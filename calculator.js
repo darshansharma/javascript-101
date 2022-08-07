d53ffe70d9fa1a632cd699d59ee933bc22b2459a
sanjeevprabhakar <sanjeevprabhakar2021@gmail.com>
2022-08-06 19:56:50 -0600

    working for all

--- calculator.js
+++ calculator.js
@@ -1,4 +1,5 @@
 var one;
+var operator;
 
 function setvalue(val) {
     document.getElementById("tagone").value += val;
@@ -6,41 +7,64 @@ function setvalue(val) {
 
 function plusfun() {
     one = parseInt(document.getElementById("tagone").value);
-    var h = "+";
+    operator = "+";
     document.getElementById("tagone").value = "";
-    document.getElementById("tagone").value += h;
+
 }
 
 function minusfun() {
     one = parseInt(document.getElementById("tagone").value);
-    var i = "-";
+    operator = "-";
     document.getElementById("tagone").value = "";
-    document.getElementById("tagone").value += i;
+
 }
 
 function dividefun() {
     one = parseInt(document.getElementById("tagone").value);
-    var j = "/";
+    operator = "/";
     document.getElementById("tagone").value = "";
-    document.getElementById("tagone").value += j;
+
 }
 
 function multiplyfun() {
     one = parseInt(document.getElementById("tagone").value);
+    operator = "*";
     document.getElementById("tagone").value = "";
 }
 
 function percentfun() {
     one = parseInt(document.getElementById("tagone").value);
-
+    operator = "%";
     document.getElementById("tagone").value = "";
 }
 
 function equalfun() {
     var two = parseInt(document.getElementById("tagone").value);
-    document.getElementById("tagone").value = one + two;
+    if (operator == "+") {
+        document.getElementById("tagone").value = one + two;
+    }
+
+    if (operator == "-") {
+        document.getElementById("tagone").value = one - two;
+    }
+
+    if (operator == "/") {
+        document.getElementById("tagone").value = one / two;
+    }
+
+    if (operator == "*") {
+        document.getElementById("tagone").value = one * two;
+    }
+
+    if (operator == "%") {
+        document.getElementById("tagone").value = one % two;
+    }
+
+
+
+
 }
 
 function clearfun() {
-    document.getElementById("tagone").value = " ";
+    document.getElementById("tagone").value = "";
 }
