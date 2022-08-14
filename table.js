9b1bff6f86ed56514dc426f43de6c60b47c1d19a
sanjeevprabhakar <sanjeevprabhakar2021@gmail.com>
2022-08-13 21:37:51 -0600

    insert values in tables

---
+++ table.js
@@ -0,0 +1,15 @@
+var data = {
+    "data": [{ "firstName": "Darshan", "lastName": "Sharma", "age": 24 },
+    { "firstName": "Sanjeev", "lastName": "Prabhakar", "age": 20 },
+    { "firstName": "Virat", "lastName": "Kohli", "age": 32 },
+    { "firstName": "Rohit", "lastName": "Sharma", "age": 34 },
+    { "firstName": "M.S", "lastName": "Dhoni", "age": 41 },
+    { "firstName": "Yuvraj", "lastName": "Singh", "age": 39 },], "message": "data fetched successfully"
+};
+
+
+for(var i = 0 ; i < 6; i++){
+    console.log(data["data"][i]["firstName"]);
+    console.log(data["data"][i]["lastName"]);
+    console.log(data["data"][i]["age"]);
+}
