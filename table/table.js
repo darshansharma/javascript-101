f7d12c74f82b42f2232d4190e2bb938628fe9d98
sanjeevprabhakar <sanjeevprabhakar2021@gmail.com>
2022-08-27 19:52:18 -0600

    print value using function

--- table/table.js
+++ table/table.js
@@ -56,31 +56,54 @@ function getMoreData() {
     });
 }
 
-let arr1 = [];
+
+function insertfun(){
+
+    let arr1 = [];
+    for (let i = 0; i < store.length; i++) {
+        arr1.push(store[i]);
+    }
+
+    for (let i = 0; i < bigstorage.length; i++) {
+        arr1.push(bigstorage[i]);
+    }
+
+
+    let table = document.getElementById("mainTable");
+    for (let i = 0; i < arr1.length; i++) {
+        let row = table.insertRow();
+        row.insertCell().append(arr1[i].firstName);
+        row.insertCell().append(arr1[i].lastName);
+        row.insertCell().append(arr1[i].age);
+    }
+
+}
+
+
+
+
+
+
+function moreinsertfun(){
+
+}
+
+let store;
+let bigstorage;
+
 
 getData()
     .then(function (resp) {
-        let contain = resp["data"];
+        store = resp["data"];
         console.log("First promise resolved");
-        for (let i = 0; i < contain.length; i++) {
-            arr1.push(contain[i]);
-        }
-        console.log(contain);
+       
+        console.log(store);
         getMoreData()
             .then(function (value) {
                 console.log("Second promise resolved");
-                let container = value["data"];
-                console.log(container);
-                for (let i = 0; i < container.length; i++) {
-                    arr1.push(container[i]);
-                }
-                let table = document.getElementById("mainTable");
-                for (let i = 0; i < arr1.length; i++) {
-                    let row = table.insertRow();
-                    row.insertCell().append(arr1[i].firstName);
-                    row.insertCell().append(arr1[i].lastName);
-                    row.insertCell().append(arr1[i].age);
-                }
+                bigstorage = value["data"];
+                console.log(bigstorage);
+              insertfun();
                 console.log("Final array = ", arr1);
             })
             .catch(function (error) {
