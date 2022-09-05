40e2e75af2f749a458812f0fa6ef48555f7ba707
sanjeevprabhakar <sanjeevprabhakar2021@gmail.com>
2022-09-04 21:00:21 -0600

    multipication

---
+++ 2darray/matrixmultipication.js
@@ -0,0 +1,32 @@
+let arr1 = [3][3];
+let arr2 = [3][3];
+let arr3 = [3][3];
+
+let a = 1;
+let b = 2;
+
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        arr1[i][j] = a;
+        a++;
+    }
+
+}
+
+
+
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        arr2[i][j] = b;
+        b++;
+    }
+
+}
+
+
+
+
+
+
+
+
