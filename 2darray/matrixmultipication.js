744f7b59c2502d4a99c6aa3456be42c74c125042
Darshan Sharma <thedarshansharma@gmail.com>
2022-09-06 09:14:19 +0530

    declaration of array issue fixed

--- 2darray/matrixmultipication.js
+++ 2darray/matrixmultipication.js
@@ -1,6 +1,6 @@
-let arr1 = [3][3];
-let arr2 = [3][3];
-let arr3 = [3][3];
+let arr1 = [[], [], []];
+let arr2 = [[], [], []];
+let arr3 = [[], [], []];
 
 let a = 1;
 let b = 2;
@@ -11,8 +11,6 @@ for (let i = 0; i < 3; i++) {
         a++;
     }
 }
-
-
 
 for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
@@ -27,16 +25,8 @@ for (let i = 0; i < 3; i++) {
     }
 }
 
-
 for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-       console.log(arr3[i][j]);
+        console.log(arr3[i][j]);
     }
 }
-
-
-
-
-
-
-
