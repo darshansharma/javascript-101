5bb25796b73bf5f1f66a49d712fa3e6fde522fad
sanjeevprabhakar <sanjeevprabhakar2021@gmail.com>
2022-09-07 20:36:16 -0600

    multipication of two matrices

--- 2darray/matrixmultipication.js
+++ 2darray/matrixmultipication.js
@@ -22,7 +22,7 @@ for (let i = 0; i < 3; i++) {
 for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
         for (let k = 0; k < 3; k++) {
-            arr3[i][j] = arr3[i][j] + arr1[i][k] * arr2[k][i];
+            arr3[i][j] = arr3[i][j] + arr1[i][k] * arr2[k][j];
         }
     }
 }
