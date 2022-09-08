a2265ce54a6895cc1c24d2a265944222351e7ae3
sanjeevprabhakar <sanjeevprabhakar2021@gmail.com>
2022-09-07 20:25:17 -0600

    multipication of two matrices using nested for loop

--- 2darray/matrixmultipication.js
+++ 2darray/matrixmultipication.js
@@ -21,9 +21,13 @@ for (let i = 0; i < 3; i++) {
 
 for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-        arr3[i][j] = arr1[i][j] + arr2[i][j];
+        for (let k = 0; k < 3; k++) {
+            arr3[i][j] = arr3[i][j] + arr1[i][k] * arr2[k][i];
+        }
     }
 }
+
+
 
 for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
